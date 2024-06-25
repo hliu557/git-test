6dbb4f51e7d67bd5ae76f0de0f26d9f4500f6ecd
hliu557 <2509841336@qq.com>
2024-06-25 22:28:49 +0800

    `12`

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import com.lhy.anoTest.*;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String hexCode = "8431C5A3";
+        String hexCode2 = "ff";
+        byte[] bytes = HexToBytesConverter.hexToBytes(hexCode);
+        byte[] bytes2 = HexToBytesConverter.hexToBytes(hexCode2);
+
+
+        String str = new String(bytes, "GB18030");
+        String str2 = new String(bytes2,"GBK");
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str.equals(str2));
+
+
+    }
+
+
+}
