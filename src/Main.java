2f7ff3b71869e0bad85d8e3eab2ea2c777f19f34
hliu557 <2509841336@qq.com>
2024-07-06 12:04:27 +0800

    t3

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,8 @@ public class Main {
         System.out.println(str);
         System.out.println(str2);
         System.out.println(str.equals(str2));
+        System.out.println(2);
+        System.out.println(998);
 
 
     }
